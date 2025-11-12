88ce3abb35378eab15da7cce7e5358e66648cee4
theapana <>
2025-11-11 17:37:08 -0800

    add more chart entries

--- src/App.js
+++ src/App.js
@@ -9,48 +9,72 @@ function IPAChart() {
     return <table className="IPAChart">
         <tbody>
             <tr>
+                <td></td>
+                <td colspan="4">Labial</td>
+                <td colspan="8">Coronal</td>
+                <td colspan="2"></td>
+                <td colspan="4">Dorsal</td>
+                <td colspan="6">Radical</td>
+            </tr>
+            <tr>
                 <td>plosive</td>
+                <IPAEntry value="" />
+                <IPAEntry value="" />
                 <IPAEntry value="p" />
                 <IPAEntry value="b" />
-                <IPAEntry value="t" />
-                <IPAEntry value="d" />
+                <IPAEntry value="t" colspan="3"/>
+                <IPAEntry value="d" colspan="3"/>
+                <IPAEntry value="ʈ" />
+                <IPAEntry value="ɖ" />
                 <IPAEntry value="c" />
                 <IPAEntry value="ɟ" />
                 <IPAEntry value="k" />
                 <IPAEntry value="g" />
+                <IPAEntry value="q" />
+                <IPAEntry value="ɢ" />
             </tr>
             <tr>
                 <td>fricative</td>
+                <IPAEntry value="ɸ" />
+                <IPAEntry value="ʙ" />
                 <IPAEntry value="f" />
                 <IPAEntry value="v" />
                 <IPAEntry value="θ" />
                 <IPAEntry value="ð" />
-                <IPAEntry value="ʈ" />
-                <IPAEntry value="ɖ" />
                 <IPAEntry value="s" />
                 <IPAEntry value="z" />
                 <IPAEntry value="ʃ" />
                 <IPAEntry value="ʒ" />
                 <IPAEntry value="ʂ" />
                 <IPAEntry value="ʐ" />
+                <IPAEntry value="c" />
+                <IPAEntry value="ɟ" />
+                <IPAEntry value="x" />
+                <IPAEntry value="ɣ" />
+                <IPAEntry value="χ" />
+                <IPAEntry value="" />
+            </tr>
+            <tr>
+                <td>liquid</td>
             </tr>
             <tr>
                 <td>nasal</td>
-                <IPAEntry value="m" />
-                <IPAEntry value="ɱ" />
-                <IPAEntry value="n" />
-                <IPAEntry value="ɳ" />
-                <IPAEntry value="ɲ" />
-                <IPAEntry value="ŋ" />
-                <IPAEntry value="ɴ" />
+                <IPAEntry value="m" colspan="2"/>
+                <IPAEntry value="ɱ" colspan="2"/>
+                <IPAEntry value="n" colspan="6"/>
+                <IPAEntry value="ɳ" colspan="2"/>
+                <IPAEntry value="ɲ" colspan="2"/>
+                <IPAEntry value="ŋ" colspan="2"/>
+                <IPAEntry value="ɴ" colspan="2"/>
             </tr>
         </tbody>
     </table>;
 }
 
-function IPAEntry({value}) {
+function IPAEntry({value, colspan}) {
     const [hidden, setHidden] = useState(true);
     return <td
+        colspan={colspan}
         onMouseEnter={() => setHidden(false)}
         onMouseLeave={() => setHidden(true)}>
         {value}
@@ -88,5 +112,5 @@ function Key({value, hidden}) {
         "ŋ": ["+", "ŋ"],
         "ɴ": ["+", "ɴ"],
     };
-    return !hidden && <div className="key-tooltip">{keymap[value]}</div>;
+    return !hidden && <div className="key-tooltip">{keymap[value] || value}</div>;
 }
