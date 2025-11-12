c569ead6cb5250cb9f7831454aa511f6f6abd6b6
theapana <>
2025-11-11 17:37:08 -0800

    chart + tooltip

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import {useState} from "react";
+export default function App() {
+    return <div>
+        <IPAChart />
+    </div>;
+}
+
+function IPAChart() {
+    return <table className="IPAChart">
+        <tbody>
+            <tr>
+                <IPAEntry value="p" />
+                <IPAEntry value="b" />
+            </tr>
+            <tr>
+                <IPAEntry value="f" />
+                <IPAEntry value="v" />
+            </tr>
+        </tbody>
+    </table>;
+}
+
+function IPAEntry({value}) {
+    const [hidden, setHidden] = useState(true);
+    return <td>
+        <div onMouseEnter={() => setHidden(false)}
+            onMouseLeave={() => setHidden(true)}>
+            {value}
+            {hidden ? null: <div className="key-tooltip">{value}</div>}
+        </div>
+    </td>;
 }
