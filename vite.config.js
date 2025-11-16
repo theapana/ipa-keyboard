0abe4aaf36921bfa219f9dfa5846e2f3cbd7d8c6
theapana <>
2025-11-15 19:26:45 -0800

    deploy pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(() => {
     return {
+        base: "ipa-keyboard",
         build: {
             outDir: 'build',
         },
