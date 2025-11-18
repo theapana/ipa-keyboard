aa87c99e04af6111304cd7b5b58d643a4267b67b
theapana <>
2025-11-18 11:35:19 -0800

    move keymap to own file

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,6 +2,7 @@ import ReactDOMServer from 'react-dom/server';
 import Keyboard from 'react-simple-keyboard';
 import 'react-simple-keyboard/build/css/index.css';
 
+import {keymap} from "./keymap";
 import Key from "./Key";
 
 export default function Layout() {
@@ -25,43 +26,6 @@ function Label({_}) {
         <div className="key-label retroflex">{_["retroflex"]}</div>
     </div>;
 }
-
-const keymap = {
-    "p": {"base": "p"},
-    "b": {"base": "b", "raise": "β", "uvular": "ʙ", "implosive": "ɓ"},
-    "t": {"base": "t", "lower": "θ", "retroflex": "ʈ"},
-    "d": {"base": "d", "lower": "ð", "retroflex": "ɖ", "implosive": "ɗ"},
-    "c": {"base": "c", "lower": "ç", "raise": "ɕ"},
-    "j": {"base": "j", "slash": "ɟ", "raise": "ʝ", "lateral": "ʎ"},
-    "k": {"base": "k"},
-    "g": {"base": "g", "raise": "ɣ", "uvular": "ɢ", "implosive": "ɠ"},
-    "q": {"base": "q", "implosive": "ʛ"},
-    "f": {"base": "f", "lower": "ɸ"},
-    "v": {"base": "v", "raise": "ʋ"},
-    "s": {"base": "s", "lower": "ʃ", "retroflex": "ʂ", "implosive": "ʄ"},
-    "z": {"base": "z", "lower": "ʒ", "raise": "ʑ", "retroflex": "ʐ"},
-    "x": {"base": "x", "raise": "χ"},
-    "h": {"base": "h", "raise": "ɦ", "lower": "ħ", "uvular": "ʜ"},
-    "r": {"base": "r", "lower": "ɹ", "retroflex": "ɻ", "uvular": "ʁ"},   // fricative/approximant
-    "R": {"base": "ɾ", "retroflex": "ɽ", "lateral": "ɺ", "uvular": "ʀ"}, // tap/flap/trill
-    "l": {"base": "l", "retroflex": "ɭ", "uvular": "ʟ"},
-    "w": {"base": "w", "lower": "ʍ"}, // approximant
-    "W": {"base": "ɯ", "lower": "ɰ"}, // vowel/semi-vowel
-    "m": {"base": "m", "raise": "ɱ"},
-    "n": {"base": "n", "raise": "ɲ", "retroflex": "ɳ", "velar": "ŋ", "uvular": "ɴ"},
-    "a": {"base": "a", "raise": "ɑ", "round": "ɒ"},
-    "i": {"base": "i", "slash": "ɨ", "lower": "ɪ"},
-    "u": {"base": "u", "slash": "ʉ", "lower": "ʊ"},
-    "y": {"base": "y", "lower": "ʏ"},
-    "e": {"base": "e", "lower": "ɛ"},
-    "o": {"base": "o", "slash": "ø", "lower": "ɔ", "round": "ɤ"},
-    "'": {"base": "'", "lower": "ˈ"}, // apostrophe => primary stress
-    ",": {"base": ",", "lower": "ˌ"}, // comma => secondary stress
-    ";": {"base": ";", "lower": "ː"}, // semicolon => length
-    // "œ": {"base": "o"},
-    // "ʌ": {"base": "o"},
-    // "ɶ": {"base": "O"},
-};
 
 function IPAKeyboard() {
     // https://react.dev/reference/react/Children#alternatives
---
+++ src/keymap.jsx
@@ -0,0 +1,36 @@
+export const keymap = {
+    "p": {"base": "p"},
+    "b": {"base": "b", "raise": "β", "uvular": "ʙ", "implosive": "ɓ"},
+    "t": {"base": "t", "lower": "θ", "retroflex": "ʈ"},
+    "d": {"base": "d", "lower": "ð", "retroflex": "ɖ", "implosive": "ɗ"},
+    "c": {"base": "c", "lower": "ç", "raise": "ɕ"},
+    "j": {"base": "j", "slash": "ɟ", "raise": "ʝ", "lateral": "ʎ"},
+    "k": {"base": "k"},
+    "g": {"base": "g", "raise": "ɣ", "uvular": "ɢ", "implosive": "ɠ"},
+    "q": {"base": "q", "implosive": "ʛ"},
+    "f": {"base": "f", "lower": "ɸ"},
+    "v": {"base": "v", "raise": "ʋ"},
+    "s": {"base": "s", "lower": "ʃ", "retroflex": "ʂ", "implosive": "ʄ"},
+    "z": {"base": "z", "lower": "ʒ", "raise": "ʑ", "retroflex": "ʐ"},
+    "x": {"base": "x", "raise": "χ"},
+    "h": {"base": "h", "raise": "ɦ", "lower": "ħ", "uvular": "ʜ"},
+    "r": {"base": "r", "lower": "ɹ", "retroflex": "ɻ", "uvular": "ʁ"},   // fricative/approximant
+    "R": {"base": "ɾ", "retroflex": "ɽ", "lateral": "ɺ", "uvular": "ʀ"}, // tap/flap/trill
+    "l": {"base": "l", "retroflex": "ɭ", "uvular": "ʟ"},
+    "w": {"base": "w", "lower": "ʍ"}, // approximant
+    "W": {"base": "ɯ", "lower": "ɰ"}, // vowel/semi-vowel
+    "m": {"base": "m", "raise": "ɱ"},
+    "n": {"base": "n", "raise": "ɲ", "retroflex": "ɳ", "velar": "ŋ", "uvular": "ɴ"},
+    "a": {"base": "a", "raise": "ɑ", "round": "ɒ"},
+    "i": {"base": "i", "slash": "ɨ", "lower": "ɪ"},
+    "u": {"base": "u", "slash": "ʉ", "lower": "ʊ"},
+    "y": {"base": "y", "lower": "ʏ"},
+    "e": {"base": "e", "lower": "ɛ"},
+    "o": {"base": "o", "slash": "ø", "lower": "ɔ", "round": "ɤ"},
+    "'": {"base": "'", "lower": "ˈ"}, // apostrophe => primary stress
+    ",": {"base": ",", "lower": "ˌ"}, // comma => secondary stress
+    ";": {"base": ";", "lower": "ː"}, // semicolon => length
+    // "œ": {"base": "o"},
+    // "ʌ": {"base": "o"},
+    // "ɶ": {"base": "O"},
+};
